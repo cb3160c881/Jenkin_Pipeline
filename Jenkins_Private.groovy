c3d3f862cfbdfc1298049eeadc4d3d7a8eb10681
Chaitanya <Chaitanya.bhargav19@gmail.com>
2022-02-15 01:35:53 +0530

    Added Clone step

--- Jenkins_Private.groovy
+++ Jenkins_Private.groovy
@@ -4,6 +4,9 @@ pipeline{
         stage("Clone"){
             steps{
                 println "We download source code from private repo"
+
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/cb3160c881/Private_Repo.git']]])
+                
             }
         }
         stage("Build"){
