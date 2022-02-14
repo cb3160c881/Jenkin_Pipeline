c9bdc4abf33e02297d5002c5e168dc13f524019e
Chaitanya <Chaitanya.bhargav19@gmail.com>
2022-02-15 00:15:43 +0530

    Added Upload Commands

--- Jenkins_Build.groovy
+++ Jenkins_Build.groovy
@@ -11,6 +11,7 @@ pipeline{
         stage("Upload"){
             steps{
                 println "Here we will upload Artifacts to S3 Bucket"
+                sh "aws s3 cp target/hello-*.war s3://pipelineupload01"
             }
         }
         stage("Deploy"){
